7b957beaad10491e99f87b70b3d29757d3bef1db
Yo Mizuki <ymizuki0530@gmail.com>
2024-10-18 01:17:28 +0900

    DetailLogPage:　detailTypeとdetailIdを取得。

--- src/main/resources/static/JS/detail/fetchDetailData.js
+++ src/main/resources/static/JS/detail/fetchDetailData.js
@@ -1,6 +1,16 @@
-$(document).ready(function(){
-	fetchAndRenderData('List');
-});
-function changeList(listName){
-	fetchAndRenderData(listName)
-}
+
+let dataType = document.getElementById('dataType').textContent;
+let dataId = document.getElementById('dataId').textContent;
+
+// Output the values to the browser console
+console.log('Data Type:', dataType);
+console.log('Data ID:', dataId);
+
+
+//$(document).ready(function() {
+//	fetchAndRenderData('List');
+//});
+//
+//function changeList(listName) {
+//	fetchAndRenderData(listName);
+//}

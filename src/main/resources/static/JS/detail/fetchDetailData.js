1c99785171d0f76b78cd2718506af5ce5bd5c71d
Yo Mizuki <ymizuki0530@gmail.com>
2024-10-19 00:11:53 +0900

    DetailLog: JS 分岐テスト成功。
    無事にGENREとBOOKが表示される。

--- src/main/resources/static/JS/detail/fetchDetailData.js
+++ src/main/resources/static/JS/detail/fetchDetailData.js
@@ -1,12 +1,18 @@
-
 let dataType = document.getElementById('dataType').textContent.replace(' Log','');
 let dataId = document.getElementById('dataId').textContent;
 
-
 $(document).ready(function() {
-	fetchDetailData('List',dataType, dataId);
+	if (dataType == 'GENRE') {
+		fetchGenreDetailData('List', dataId);
+	} else {
+		fetchBookDetailData('List', dataId);
+	}
 });
 
 function changeDetailList(listName, dataType, dataId) {
-	fetchDetailData(listName, dataType, dataId);
+	if (dataType == 'GENRE') {
+		fetchGenreDetailData(listName, dataId);
+	} else {
+		fetchBookDetailData(listName, dataId);
+	}
 }

23a072c3b6418031d78e553516c910490e085dcf
Yo Mizuki <ymizuki0530@gmail.com>
2024-11-16 19:54:36 +0900

    DetailPage:全てのログの出力機能
    全てのログを拾えるように戻す。

--- src/main/java/com/example/app/mapper/detail/DetailLogByGenreMapper.java
+++ src/main/java/com/example/app/mapper/detail/DetailLogByGenreMapper.java
@@ -8,6 +8,8 @@ import com.example.app.domain.StudyLog;
 
 @Mapper
 public interface DetailLogByGenreMapper {
-    List<StudyLog> getDetailLogGenreDays(Integer userId, Integer dataId, Integer days) throws Exception;
-    List<StudyLog> getAggregatedLogGenreDays(Integer userId, Integer dataId, Integer days) throws Exception;
+	List<StudyLog> getDetailLogGenre(Integer userId, Integer dataId) throws Exception;
+	List<StudyLog> getDetailLogGenreDays(Integer userId, Integer dataId, Integer days) throws Exception;
+	List<StudyLog> getAggregatedLogGenre(Integer userId, Integer dataId) throws Exception;
+	List<StudyLog> getAggregatedLogGenreDays(Integer userId, Integer dataId, Integer days) throws Exception;
 }
